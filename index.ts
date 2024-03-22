c8e9a919591fea9c8245591dcc633b945b039a3f
rumaisalman <rumi.salman813@gmail.com>
2024-03-23 00:34:48 +0500

    project completed

---
+++ index.ts
@@ -0,0 +1,23 @@
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10)
+
+const answers = await inquirer.prompt([
+	{
+		message: "Guess the number between 1-10!",
+		type: "number",
+		name: "guessedNumber"
+	},])
+
+console.log(answers)
+
+if (answers.guessedNumber < randomNumber) {
+	console.log("Oops, You number is too small. Try again.")
+} else if (answers.guessedNumber > randomNumber) {
+	console.log("Oops,Your number is too big.Try again.")
+} else if (answers.guessedNumbers == randomNumber) {
+	console.log("Yay! You have guessed the correct number!")
+} else {
+	console.log("Please enter a valid number.")
+}
+
